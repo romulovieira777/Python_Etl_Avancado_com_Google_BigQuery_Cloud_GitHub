ea78e56c818917ed679a7bdda5d5764ea0e3fc7c
Romulo Vieira <romulo.vieira777@gmail.com>
2024-07-15 17:37:39 -0300

    Introdução

---
+++ Secao_01_Introducao/Codigo_SQL/01_Select.sql
@@ -0,0 +1,44 @@
+SELECT
+	*
+FROM
+	public.clientes;
+
+
+SELECT
+    *
+FROM
+    public.clientes
+LIMIT 1000;
+
+
+SELECT
+    last_name
+  , email
+  , state
+FROM
+    public.clientes
+LIMIT 1000;
+
+
+SELECT
+    first_name
+  , email
+  , last_name
+FROM
+    public.clientes;
+
+
+SELECT
+    *
+FROM
+    public.clientes;
+
+
+SELECT
+    id
+  , created_at
+  , first_name
+  , email
+  , cell_phone
+FROM
+    public.clientes;
